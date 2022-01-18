a714364bcf4d93a69048e4f2f595b54383397279
palashg <palashguptakatni@gmail.com>
2022-01-18 23:11:34 +0530

    Router support added for Home and about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import { v4 as uuidv4} from 'uuid'
+import AboutPage from './pages/AboutPage'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import AboutIconLink from './components/AboutIconLink'
 
 function App() {
     const [feedback,setFeedback] = useState(FeedbackData)
@@ -21,15 +24,26 @@ function App() {
     }
 
     return (
-        <>
-            <Header />
-            <div className="container">
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-            </div>
-        </>
-    )
+      <Router>
+        <Header />
+        <div className="container">
+            <Routes>
+                <Route exact path='/' element={
+                    <>
+                        <FeedbackForm handleAdd={addFeedback} />
+                        <FeedbackStats feedback={feedback} />
+                        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                    </>
+                }>    
+                </Route>  
+
+                <Route path='/about' element={<AboutPage />} /> 
+            </Routes>
+
+            <AboutIconLink />
+        </div>
+      </Router>
+    );
 }
 
 export default App
