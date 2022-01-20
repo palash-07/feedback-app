b3ab8f4c1afc431bc90aad456fdd3a8b887c8fff
palashg <palashguptakatni@gmail.com>
2022-01-20 12:51:35 +0530

    context API support for global state feedback (eliminates prop-drilling)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,12 +2,16 @@ import Card from "./shared/Card"
 import {useState} from 'react'
 import Button from "./shared/Button"
 import RatingSelect from "./RatingSelect"
+import {useContext} from 'react'
+import FeedbackContext from "../context/FeedbackContext"
 
-function FeedbackForm({handleAdd}) {
+function FeedbackForm() {
     const [text,setText] = useState('')
     const [btnDisabled,setBtnDisabled] = useState(true)
     const [message,setMessage] = useState('')
     const [rating,setRating] = useState(10)
+
+    const {addFeedback} = useContext(FeedbackContext)
 
     const handleTextChange = (event) => {
         if (text === ''){
@@ -34,7 +38,7 @@ function FeedbackForm({handleAdd}) {
                 rating
             }
             
-            handleAdd(newFeedback)
+            addFeedback(newFeedback)
             setText('')
         }
     }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
 import {motion, AnimatePresence} from 'framer-motion'
+import {useContext} from 'react'
+import FeedbackContext from "../context/FeedbackContext"
 
-function FeedbackList({feedback , handleDelete}) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+
     if (!feedback || feedback.length == 0) {
         return <p>No feedback yet</p>
     }
@@ -20,23 +23,12 @@ function FeedbackList({feedback , handleDelete}) {
               <FeedbackItem
                 key={item.id}
                 item={item}
-                handleDelete={handleDelete}
               />
             </motion.div>
           ))}
         </AnimatePresence>
       </div>
     );
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired
-        })
-    ),
 }
 
 export default FeedbackList
